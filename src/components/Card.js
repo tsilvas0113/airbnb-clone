a9d7c7beb0840127dd3771d370c7d2cc157c5a6f
Tristan <tsilvas0113@gmail.com>
2022-06-23 11:17:08 -0500

    Update card badges using conditional rendering

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 function Card (props) {
+    let badgeText
+    if (props.spots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.location === 'Online') {
+        badgeText = 'ONLINE'
+    }
     return (
         <div className="card-box">
+           {badgeText !== undefined && <div className="card-badge">{badgeText}</div>}
            <img className="katie" src={props.img} alt='Katie Zaferes' />
            <div className='rating'>
                 <img className='star' src='./images/star.png' alt='star rating' />
